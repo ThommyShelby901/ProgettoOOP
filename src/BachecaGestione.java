01ff57671183fc4848c40f99b8864e0bf6eb0f69
tommyshelby <lorenzo.esposito31@gmail.com>
2025-05-15 21:02:39 +0200

    lavoro aggiornato

---
+++ src/BachecaGestione.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class BachecaGestione {
+    private static final List<Bacheca> listaBacheche = new ArrayList<>();
+
+    public static void creaBacheca(String titolo, String descrizione) {
+        listaBacheche.add(new Bacheca(titolo, descrizione));
+    }
+
+    public static void modificaBacheca(String titoloCorrente, String nuovoTitolo, String nuovaDescrizione) {
+        for (Bacheca b : listaBacheche) {
+            if (b.getTitoloBacheca().equalsIgnoreCase(titoloCorrente)) {
+                b.setTitoloBacheca(nuovoTitolo);
+                b.setDescrizioneBacheca(nuovaDescrizione);
+                return;
+            }
+        }
+        System.out.println("Bacheca non trovata.");
+    }
+
+    public static void eliminaBacheca(String titolo) {
+        listaBacheche.removeIf(b -> b.getTitoloBacheca().equalsIgnoreCase(titolo));
+    }
+
+    public static void visualizzaBacheche() {
+        for (Bacheca b : listaBacheche) {
+            System.out.println("- " + b.getTitoloBacheca() + ": " + b.getDescrizioneBacheca());
+        }
+    }
+
+    public static List<Bacheca> getListaBacheche() {
+        return listaBacheche;
+    }
+}
