4cd652760db3b1eb361082deb13126cab8cc5b5b
tommyshelby <lorenzo.esposito31@gmail.com>
2025-07-13 13:52:04 +0200

    lavoro quasi quasi term?!?!

--- src/main/java/org/example/model/CheckList.java
+++ src/main/java/org/example/model/CheckList.java
@@ -1,73 +1,98 @@
 package org.example.model;
-public class CheckList {
 
+/**
+ * Rappresenta una voce della checklist associata a un To-Do.
+ * Ogni voce ha un ID univoco, una descrizione, uno stato di completamento e
+ * un riferimento all'id del To-Do a cui appartiene.
+ */
+public class CheckList {
     private int idCheckList;
     private String descrizione;
     private boolean completato;
-    private int idToDo;
+    int idToDo;
     private StatoCheck stato;
 
+    /**
+     * Restituisce lo stato della voce della checklist.
+     * @return stato della voce (es. INCOMPLETO, COMPLETATO)
+     */
     public StatoCheck getStato() {
         return stato;
     }
 
+    /**
+     * Imposta lo stato della voce della checklist.
+     * @param stato nuovo stato da assegnare
+     */
     public void setStato(StatoCheck stato) {
         this.stato = stato;
     }
 
-
-    public CheckList() {
-    }
-
-    // Costruttore completo
-    public CheckList(int idCheckList, String descrizione, boolean completato, int idToDo) {
-        this.idCheckList = idCheckList;
-        this.descrizione = descrizione;
-        this.completato = completato;
-        this.idToDo = idToDo;
-    }
-
-    // Costruttore senza id (per inserimento nuovo record)
-    public CheckList(String descrizione, boolean completato, int idToDo) {
-        this.descrizione = descrizione;
-        this.completato = completato;
-        this.idToDo = idToDo;
-    }
-
-    // Getters e Setters
-
+    /**
+     * Restituisce l'ID univoco della voce.
+     * @return ID della voce della checklist
+     */
     public int getIdCheckList() {
         return idCheckList;
     }
 
+    /**
+     * Imposta l'ID univoco della voce.
+     * @param idCheckList ID da assegnare
+     */
     public void setIdCheckList(int idCheckList) {
         this.idCheckList = idCheckList;
     }
 
+    /**
+     * Restituisce la descrizione della voce.
+     * @return descrizione testuale
+     */
     public String getDescrizione() {
         return descrizione;
     }
 
+    /**
+     * Imposta la descrizione della voce.
+     * @param descrizione testo da assegnare
+     */
     public void setDescrizione(String descrizione) {
         this.descrizione = descrizione;
     }
 
+    /**
+     * Indica se la voce è completata.
+     * @return true se completata, false altrimenti
+     */
     public boolean isCompletato() {
         return completato;
     }
 
+    /**
+     * Imposta il flag di completamento della voce.
+     * @param completato true per completata, false altrimenti
+     */
     public void setCompletato(boolean completato) {
         this.completato = completato;
     }
 
+    /**
+     * Imposta l'ID del To-Do a cui appartiene questa voce.
+     * @param idToDo ID del To-Do
+     */
     public void setIdToDo(int idToDo) {
         this.idToDo = idToDo;
     }
 
+    /**
+     * Rappresentazione testuale della voce della checklist,
+     * con lo stato tra parentesi quadre seguito dalla descrizione.
+     * @return stringa rappresentativa della voce
+     */
     @Override
     public String toString() {
         return "[" + stato.toString() + "] " + descrizione;
     }
-
 }
+
 
