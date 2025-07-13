3cd2bd3ee933b2d640ab219a112163b631daa5e0
tommyshelby <lorenzo.esposito31@gmail.com>
2025-07-13 13:52:56 +0200

    lavoro quasi quasi term?!?!

--- src/main/java/org/example/model/StatoCheck.java
+++ src/main/java/org/example/model/StatoCheck.java
@@ -1,7 +1,18 @@
 package org.example.model;
 
+/**
+ * Enum che rappresenta lo stato di completamento di una voce della checklist.
+ */
 public enum StatoCheck {
+    /**
+     * Stato che indica che la voce della checklist non è ancora completata.
+     */
     INCOMPLETO,
+
+    /**
+     * Stato che indica che la voce della checklist è stata completata.
+     */
     COMPLETATO
 }
+
 
